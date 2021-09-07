68316ad4e97429b2c3e92e4a262ebd6e5f39e05f
dev-khalid <khalid.hossain.akash@gmail.com>
2021-09-07 09:07:44 +0600

    started typescript with basic setup

---
+++ index.js
@@ -0,0 +1,5 @@
+// let id = 5; //type infrarance - automatically catch the type and set it to the variable . and later on if we want to assign different typed value then it will show error. 
+// id='5'
+var id = 5;
+console.log(id);
+//for watch mode tsc --watch index.ts 
